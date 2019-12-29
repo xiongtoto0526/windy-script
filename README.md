23f7c3010ddfed2e451dbbbe6a7779cbf5165303
xmaster <xionghaitao@yunlizhihui.com>
2019-12-29 16:27:42 +0800

    add feature: grb file extract 2 json

---
+++ README.md
@@ -0,0 +1,10 @@
+# WindyJS
+参考：https://github.com/andrewdamelio/WindyJS.git
+参考：https://github.com/cambecc/grib2json
+
+script目录下可以直接拉取到美国国家气象局的最新数据。
+用法：
+1. clone
+2. sh script/getWind.sh
+
+
---
+++ scripts/getWind.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DATE=$(date +'%Y%m%d'$1)
+
+# echo  "Pulling latest data for "$DATE
+# curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?dir=%2Fgfs."$DATE"&file=gfs.t"$1"z.pgrb2.1p00.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on" -o gfs.pgrb2.1p00.f000
+
+echo "Running grib2json..."
+
+bash ./bin/grib2json -d -n -o wind.json gfs.t00z.pgrb2.0p25.anl
+
+if [ ! -f wind.json ]; then
+    echo "No Wind data to download for" $DATE
+else
+  rm ./public/data/wind.json
+  cp  wind.json  ./public/data
+  rm gfs.pgrb2.1p00.f000
+  rm wind.json
+  echo "Wind data json file created @" $(date)
+fi
